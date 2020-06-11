e3868c262bacf71daa0330bd3b7669f17679035a
liuuuuug <3388941921@qq.com>
2020-06-11 08:02:45 +0800

    添加项目文件。

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_ImgLoad = new System.Windows.Forms.Button();
+            this.Btn_Save = new System.Windows.Forms.Button();
+            this.Img_box = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.Img_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_ImgLoad
+            // 
+            this.Btn_ImgLoad.Location = new System.Drawing.Point(487, 297);
+            this.Btn_ImgLoad.Name = "Btn_ImgLoad";
+            this.Btn_ImgLoad.Size = new System.Drawing.Size(159, 62);
+            this.Btn_ImgLoad.TabIndex = 0;
+            this.Btn_ImgLoad.Text = "Btn_ImgLoad";
+            this.Btn_ImgLoad.UseVisualStyleBackColor = true;
+            this.Btn_ImgLoad.Click += new System.EventHandler(this.Btn_ImgLoad_Click);
+            // 
+            // Btn_Save
+            // 
+            this.Btn_Save.Location = new System.Drawing.Point(62, 297);
+            this.Btn_Save.Name = "Btn_Save";
+            this.Btn_Save.Size = new System.Drawing.Size(152, 62);
+            this.Btn_Save.TabIndex = 1;
+            this.Btn_Save.Text = "Btn_Save";
+            this.Btn_Save.UseVisualStyleBackColor = true;
+            this.Btn_Save.Click += new System.EventHandler(this.Btn_Save_Click);
+            // 
+            // Img_box
+            // 
+            this.Img_box.Location = new System.Drawing.Point(366, 17);
+            this.Img_box.Name = "Img_box";
+            this.Img_box.Size = new System.Drawing.Size(401, 245);
+            this.Img_box.TabIndex = 2;
+            this.Img_box.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Img_box);
+            this.Controls.Add(this.Btn_Save);
+            this.Controls.Add(this.Btn_ImgLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Img_box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_ImgLoad;
+        private System.Windows.Forms.Button Btn_Save;
+        private System.Windows.Forms.PictureBox Img_box;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Data.SqlClient;
+
+using System.IO;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static SqlConnection conn;
+
+        public static SqlCommand cmd;
+
+        Stream ms;
+
+
+        private byte[] GetImageBytes(Image image)
+
+        {
+
+            Bitmap bmp = new Bitmap(image);
+
+            MemoryStream mstream = new MemoryStream();
+
+            bmp.Save(mstream, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            mstream.Seek(0, SeekOrigin.Begin); //及时定位流的开始位置
+
+            byte[] byteData = new Byte[mstream.Length];
+
+            mstream.Position = 0;
+
+            mstream.Read(byteData, 0, byteData.Length);
+
+            mstream.Close();
+
+            return byteData;
+
+        }
+
+        private void Btn_ImgLoad_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "*.jpg|*.jpg|*.bmp|*.bmp";//指定openFileDialog控件打开的文件格式
+
+
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "*jpg|*.JPG|*.GIF|*.GIF|*.BMP|*.BMP";
+
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+
+            {
+
+                if ((ms = openFileDialog1.OpenFile()) != null)
+
+                {
+
+                    //获取当前选择的图片
+
+                    this.Img_box.Image = Image.FromStream(this.openFileDialog1.OpenFile());
+
+                    //获取当前图片的路径
+
+                    string path = openFileDialog1.FileName.ToString();
+
+                    //将制定路径的图片添加到FileStream类中    
+
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+                    //通过FileStream对象实例化BinaryReader对象
+
+                    BinaryReader br = new BinaryReader(fs);
+
+                    //通过BinaryReader类对象的ReadBytes()方法将FileStream类对象转化为二进制数组
+
+                    byte[] imgBytesIn = br.ReadBytes(Convert.ToInt32(fs.Length));
+                    Img_box.SizeMode = PictureBoxSizeMode.Zoom;
+
+                }
+
+                else
+
+                {
+
+                    MessageBox.Show("您选择的图片不能被读取或文件类型错误！", "错误 ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+
+
+
+            }
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+
+            conn = new SqlConnection();
+
+            conn.ConnectionString = @"Data Source=MSSQLSERVER;Initial Catalog=BDProjectt2020;
+
+                                      Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=vs2019;Data Source=.";
+
+
+
+
+        }
+
+        private void Btn_Save_Click(object sender, EventArgs e)
+        {
+            byte[] imageBytes = GetImageBytes(Img_box.Image);
+
+
+
+            string SqlStr = "INSERT INTO DBImg(ID,Name,Img)VALUES(1,'测试图片',@ImgData)";
+
+            SqlCommand cmd = new SqlCommand(SqlStr, conn);
+
+            SqlParameter param = new SqlParameter("ImgData", SqlDbType.VarBinary, imageBytes.Length);
+
+
+            // 如果数据库关闭，则打开数据库
+
+           
+
+            cmd.Parameters.Add(param);
+
+
+            if (conn.State == ConnectionState.Closed)
+
+                try { conn.Open(); }
+
+                catch (Exception ex)
+
+                {
+                    MessageBox.Show("打开数据库失败，请检查数据设置." + ex.Message, "消息提示", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                    return;
+                }
+
+            cmd.Parameters.Add(param);
+
+
+
+            int i = cmd.ExecuteNonQuery();
+
+            MessageBox.Show(i + " 条图片数据保存成功");
+
+        }
+
+
+    }
+    }
+
+
